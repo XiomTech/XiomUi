8a01afa160d15f1a5a48f721b4b1c5d01c23b2f0
Mahamudul Hasan Miyad <mhmiyad6565@gmail.com>
2025-12-16 20:35:24 +0600

    adding  next js congic

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactCompiler: true,
+  // Exclude CLI folder from compilation
+  webpack: (config) => {
+    config.watchOptions = {
+      ...config.watchOptions,
+      ignored: ['**/src/cli/**'],
+    };
+    return config;
+  },
 };
 
 export default nextConfig;
